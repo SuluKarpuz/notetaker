e1e5b094bc531a79592ef4fd7228aa38fb6188e7
efe <e@gmail.com>
2022-12-25 14:32:31 +0300

    first commit

---
+++ NoteTaking/Form1.cs
@@ -0,0 +1,55 @@
+using System.Data;
+
+namespace NoteTaking
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title",typeof(string));
+            table.Columns.Add("Messages",typeof(string));
+
+            dataGridView1.DataSource = table;
+            dataGridView1.Columns["Messages"].Visible = false;
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMessage.Text);
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if(index > -1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        private void bttDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
+    }
+}
